464f24c4994bef54c6c7f15751736ecd9459a4cb
Your Name <you@example.com>
2022-07-27 11:22:28 -0300

    events in React

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
+const clickOnPlanet = (name) => {
+  console.log(`Um click no Planeta: ${name}`);
+};
 const Planets = () => {
   return (
     <Fragment>
@@ -11,12 +14,14 @@ const Planets = () => {
         description="Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. "
         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
         link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+        clickOnPlanet={clickOnPlanet}
       />
       <Planet
         name="Plutão"
         description="Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno."
         img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
         link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
+        clickOnPlanet={clickOnPlanet}
       />
     </Fragment>
   );
--- my-first-app/src/components/planets/planet/index.js
+++ my-first-app/src/components/planets/planet/index.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import GrayImg from "../../shared/gray_img";
 import DescriptionWithLink from "../../shared/description_with_link";
 
 const Planet = (props) => {
   return (
-    <Fragment>
+    <div onClick={() => props.clickOnPlanet(props.name)}>
       <h4>{props.name}</h4>
       <DescriptionWithLink description={props.description} link={props.link} />
       <GrayImg img_url={props.img_url} />
-    </Fragment>
+    </div>
   );
 };
 
