31aaa2cb31219de7991760cd6fa4d6b47b2fe6e3
Your Name <you@example.com>
2022-08-03 12:43:57 -0300

    class with state

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -1,26 +1,64 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
-const Planets = () => {
-  return (
-    <Fragment>
-      <h3>Planet List</h3>
-      <hr />
-      <Planet
-        name="Mercúrio"
-        description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. "
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-        link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-        title_with_underline={true}
-        gray={true}
-      />
-      <Planet
-        name="Plutão"
-        description="Plutão, formalmente designado 134340 Plutão  é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno."
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-      />
-    </Fragment>
-  );
-};
+class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planets: [
+        {
+          name: "Mercúrio",
+          description:
+            "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. ",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
+          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+        },
+        {
+          name: "Plutão",
+          description:
+            "Plutão, formalmente designado 134340 Plutão  é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[11] uma região de corpos além da órbita de Netuno.",
+          img_url:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
+          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+        },
+      ],
+    };
+  }
+
+  removeLast = () => {
+    let new_planets = [...this.state.planets];
+    new_planets.pop();
+    this.setState((state) => ({
+      planets: new_planets,
+    }));
+  };
+
+  duplicateLastPlanet = () => {
+    let last_planet = this.state.planets[this.state.planets.length - 1];
+    this.setState((state) => ({
+      planets: [...state.planets, last_planet],
+    }));
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Planet List</h3>
+        <button onClick={this.removeLast}>Remove Last</button>
+        <button onClick={this.duplicateLastPlanet}>Duplicate Last</button>
+        <hr />
+        {this.state.planets.map((planet) => (
+          <Planet
+            name={planet.name}
+            description={planet.description}
+            img_url={planet.img_url}
+            link={planet.link}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
 
 export default Planets;
