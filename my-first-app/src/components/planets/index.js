081dcc864c3a87f55d74128510e372777e9823b7
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-08-04 22:15:47 -0300

    lifecycle method

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -1,3 +1,66 @@
+<<<<<<< HEAD
+import React, { Fragment } from "react";
+import Planet from "./planet";
+
+async function getPlanets() {
+  let response = await fetch("http://localhost:3000//api/planets.json");
+  let data = await response.json();
+  return data;
+}
+
+class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planets: [],
+    };
+  }
+
+  componentDidMount() {
+    getPlanets().then((data) => {
+      this.setState((state) => ({
+        planets: data["planets"],
+      }));
+    });
+  }
+
+  removeLast = () => {
+    let new_planets = [...this.state.planets];
+    new_planets.pop();
+    this.setState((state) => ({
+      planets: new_planets,
+    }));
+  };
+
+  duplicateLastPlanet = () => {
+    let last_planet = this.state.planets[this.state.planets.length - 1];
+    this.setState((state) => ({
+      planets: [...state.planets, last_planet],
+    }));
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Planet List</h3>
+        <button onClick={this.removeLast}>Remove Last</button>
+        <button onClick={this.duplicateLastPlanet}>Duplicate Last</button>
+        <hr />
+        {this.state.planets.map((planet) => (
+          <Planet
+            name={planet.name}
+            description={planet.description}
+            img_url={planet.img_url}
+            link={planet.link}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
+
+export default Planets;
+=======
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
@@ -62,3 +125,4 @@ class Planets extends React.Component {
 }
 
 export default Planets;
+>>>>>>> 2e29081e6b87ab053d993bb1fe48b52dd8c9f595
